ea4cdc4200f10f519252316c76e91dc1b8ef60d2
Shivakumar1090 <ch.shivakumar491@gmail.com>
2024-07-10 01:45:48 +0530

    removed user

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,28 +6,24 @@ import ErrorPage from "../components/_Common/error_page";
 import Register from "../components/Auth/Register";
 import Login from "../components/Auth/Login";
 
-const user =  JSON.parse(localStorage.getItem('user'));
-
 export const AppRouter = createBrowserRouter([
     {
         path: '/',
         element: <RootLayout />,
         errorElement: <ErrorPage />,
-        children: user ? [
+        children: [
+
             {
                 path: '/',
                 index: true,
                 element: <Home />,
             },
-           
-        ] : [
             {
                 path: 'register',
                 element: <Register />,
             },
             {
                 path: 'login',
-                index: true,
                 element: <Login />
             },
         ],
